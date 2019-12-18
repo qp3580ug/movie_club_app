7c764055a77376799fb0ed9c68dae4812167e0f1
Nathan Walter <qp3580ug@go.minnstate,edu>
2019-12-17 23:18:54 -0600

    Fixed api problems, radius for search was too small

--- movie_club_app/models.py
+++ movie_club_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-import requests
 
 User._meta.get_field('email')._unique = True
 
@@ -13,12 +12,12 @@ class Film(models.Model):
     title = models.CharField(max_length=200, blank=False, unique=True)
     synopsis = models.CharField(max_length=500, blank=False)
     rating = models.CharField(max_length=1, blank=False)
-    
+
     def __str__(self):
         return "Film: " + self.title
 
 class Cafe(models.Model):
-    name = models.CharField(max_length=200, blank=False, unique=True)
+    name = models.CharField(max_length=200, blank=False)
     address = models.CharField(max_length=200, blank=False)
 
     def __str__(self):
--- movie_club_app/yelp_api.py
+++ movie_club_app/yelp_api.py
@@ -3,9 +3,10 @@ from .models import Cafe
 from django.http import HttpResponse
 import requests
 from django.shortcuts import redirect
-
+import json
 
 def searchYelp():
+    #yelpList = {}
     YELP_API_KEY = 'f3n-U3oZBl9eE1_a6_PNLZjs0Phcgs0zQDdaVvuMYq8dBntIB1h5yU9b2-xqBb-FD_i3gPqWY0Mx-BkkITo-V8uQ2LQ5cTXyFAiGn57FuHeSmoMBJFDJ3HwGRumoXXYx'
 
     yelp_url = 'https://api.yelp.com/v3/businesses/search'
@@ -15,9 +16,9 @@ def searchYelp():
     params = {
         'categories': 'coffee',
         'location': '55318',
-        'radius': '1000',
+        'radius': '40000',
         'limit': 20,
-        'sort_by': 'distance'
+        'sort_by': 'rating'
     }
 
     response = requests.get(yelp_url, headers=headers, params=params).json()
@@ -29,8 +30,16 @@ def searchYelp():
         location = c['location']
         address = ','.join(location['display_address'])
         
-        Cafe.objects.create(name = name, address = address)
-        '''cafe = Cafe(name = name, address = address)
-        cafe.save()'''
+
+        cafe = Cafe(name = name, address = address)
+        cafe.save()
+        #newEntry = {name: { "address": address }}
+            
+        #yelpList.update(newEntry)
+        
+    #new_cafes = Cafe(**yelpList)
     
-    return redirect('movie_club_app:homepage')
+    #new_cafes.save()
+    
+    
+    
